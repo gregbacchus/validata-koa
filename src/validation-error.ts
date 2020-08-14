3758106bf54c4b26ebea923d6feef73b5d471d48
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2020-08-14 20:01:26 +1200

    chore: use ValidationError from validata

--- src/validation-error.ts
+++ src/validation-error.ts
@@ -1,11 +1,1 @@
-import { Issue } from 'validata';
-
-export class ValidationError extends Error {
-  constructor(public readonly issues: Issue[]) {
-    super('Validation failed');
-
-    this.name = 'ValidationError';
-    // Set the prototype explicitly.
-    Object.setPrototypeOf(this, new.target.prototype);
-  }
-}
+export { ValidationError } from 'validata';
